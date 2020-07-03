b10c8bdc16b45205599b6d8ef9e1b6e61f8e55bc
조경진 <kj_cho@idealidea.co.kr>
2020-07-03 22:11:54 +0900

    가위바위보 -> 결과까지 얻는 코드

--- app/src/main/java/kr/co/namu/testyourluck/RockPaperScissorsActivity.kt
+++ app/src/main/java/kr/co/namu/testyourluck/RockPaperScissorsActivity.kt
@@ -2,6 +2,7 @@ package kr.co.namu.testyourluck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_rock_paper_scissors.*
 
 class RockPaperScissorsActivity : AppCompatActivity() {
@@ -9,14 +10,17 @@ class RockPaperScissorsActivity : AppCompatActivity() {
 //    그림파일 id값들 저장할 목록
     val imgList = ArrayList<Int>()
 
+//    컴퓨터가 뭘 냈는지 저장
+    var cpuPick = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rock_paper_scissors)
 
 //        목록에 그림 세장 추가 (가위 / 바위 / 보)
         imgList.add(R.drawable.scissor)
-        imgList.add(R.drawable.paper)
         imgList.add(R.drawable.rock)
+        imgList.add(R.drawable.paper)
 
         scissorBtn.setOnClickListener {
 
@@ -27,8 +31,13 @@ class RockPaperScissorsActivity : AppCompatActivity() {
             val randomNum = (Math.random() * 3).toInt()
             val thisTimePick = imgList[randomNum]
 
+
 //            랜덤으로 뽑힌 그림을 띄워주기
             cpuImg.setImageResource(thisTimePick)
+
+
+            cpuPick = randomNum
+            getResultOfRPS(0)
 
         }
 
@@ -37,13 +46,65 @@ class RockPaperScissorsActivity : AppCompatActivity() {
             val thisTimePick = imgList[randomNum]
             cpuImg.setImageResource(thisTimePick)
 
+
+            cpuPick = randomNum
+            getResultOfRPS(1)
+
         }
 
         paperBtn.setOnClickListener {
             val randomNum = (Math.random() * 3).toInt()
             val thisTimePick = imgList[randomNum]
             cpuImg.setImageResource(thisTimePick)
+
+            cpuPick = randomNum
+            getResultOfRPS(2)
         }
 
     }
+
+//    승/무/패를 판정해주는 기능
+
+    fun getResultOfRPS(myPick : Int) {
+
+//      사용자가 가위를 냈다
+        if (myPick == 0) {
+
+            if (cpuPick == 0) {
+                Toast.makeText(this, "무승부", Toast.LENGTH_SHORT).show()
+            }
+            else if (cpuPick == 1) {
+                Toast.makeText(this, "패배", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "승리", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        else if (myPick == 1) {
+//            바위를 낸 상황
+            if (cpuPick == 0) {
+                Toast.makeText(this, "승리", Toast.LENGTH_SHORT).show()
+            }
+            else if (cpuPick == 1) {
+                Toast.makeText(this, "무승부", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "패배", Toast.LENGTH_SHORT).show()
+            }
+        }
+        else {
+//            보를 낸 상황
+            if (cpuPick == 0) {
+                Toast.makeText(this, "패배", Toast.LENGTH_SHORT).show()
+            }
+            else if (cpuPick == 1) {
+                Toast.makeText(this, "승리", Toast.LENGTH_SHORT).show()
+            }
+            else {
+                Toast.makeText(this, "무승부", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 }
