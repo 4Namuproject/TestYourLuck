d33bac29517dd106af9611196828a96f9d9e2ce7
조경진 <kj_cho@idealidea.co.kr>
2020-07-03 20:05:23 +0900

    인원수 버튼 동작

--- app/src/main/java/kr/co/namu/testyourluck/LotsActivity.kt
+++ app/src/main/java/kr/co/namu/testyourluck/LotsActivity.kt
@@ -2,6 +2,7 @@ package kr.co.namu.testyourluck
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_lots.*
 
 class LotsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +10,31 @@ class LotsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_lots)
 
         title = "제비 뽑기"
+
+        peopleCountMinusBtn.setOnClickListener {
+
+//            지금 몇명인지 받아오기
+            var peopleCount = peopleCountTxt.text.toString().toInt()
+
+//            1명 줄여주기
+            peopleCount--
+
+//            인원수 문구 변경
+            peopleCountTxt.text = peopleCount.toString()
+
+        }
+
+        peopleCountPlusBtn.setOnClickListener {
+            //            지금 몇명인지 받아오기
+            var peopleCount = peopleCountTxt.text.toString().toInt()
+
+//            1명 늘려주기
+            peopleCount++
+
+//            인원수 문구 변경
+            peopleCountTxt.text = peopleCount.toString()
+        }
+
 
     }
 }
